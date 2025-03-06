2c20e48857169169a3eced232c1bbf548e1c66c2
christopheresbray <chris.bray@braychan.com.au>
2025-03-06 19:21:15 +1030

    Committing latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'date-fns/_lib/format/longFormatters': 'date-fns/esm/_lib/format/longFormatters'
+    }
+  },
+  optimizeDeps: {
+    include: ['date-fns/esm/_lib/format/longFormatters']
+  }
 })
