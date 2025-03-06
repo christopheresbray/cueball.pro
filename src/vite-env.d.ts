9bc74d5a1614eb6f0e3b7b1a752c1a9231cf0e2c
christopheresbray <chris.bray@braychan.com.au>
2025-03-06 19:39:33 +1030

    Committing latest changes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,16 @@
 /// <reference types="vite/client" />
 
-interface ImportMeta {
-    readonly env: Record<string, string>;
+interface ImportMetaEnv {
+    readonly VITE_FIREBASE_API_KEY: string;
+    readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+    readonly VITE_FIREBASE_PROJECT_ID: string;
+    readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+    readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly VITE_FIREBASE_APP_ID: string;
+    readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+  }
+  
+  // This ensures that the ImportMeta interface from Vite includes our custom env variables
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
   }
